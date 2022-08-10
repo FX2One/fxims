08c0effb1b83db41d28b04c3adf8f4aa80132ee5
FX2One <dfoxik@gmail.com>
2022-08-11 00:25:59 +0200

    checking against admin site login with new fixture that creates entry to db

--- fxecommerce/dashboard/tests/test_selenium_dashboard.py
+++ fxecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -4,12 +4,12 @@ from selenium.webdriver.common.by import By
 
 
 
-@pytest.mark.selenium
+'''@pytest.mark.selenium
 def test_new_admin_user(create_admin):
-    assert create_admin.username == "admin"
+    assert create_admin.username == "admin"'''
 
 @pytest.mark.selenium
-def test_dashboard_admin_login(live_server, chrome_browser, create_admin):
+def test_dashboard_admin_login(live_server, chrome_browser, django_database_fixture):
     driver = chrome_browser
     driver.get(f'{live_server.url}/admin/login/')
 
@@ -17,7 +17,7 @@ def test_dashboard_admin_login(live_server, chrome_browser, create_admin):
     user_password = driver.find_element(By.XPATH, '//*[@id="id_password"]')
     submit_button = driver.find_element(By.XPATH, '//input[@value="Log in"]')
 
-    user_name.send_keys(create_admin.username)
+    user_name.send_keys('admin')
     user_password.send_keys('password')
     submit_button.send_keys(Keys.RETURN)
 
