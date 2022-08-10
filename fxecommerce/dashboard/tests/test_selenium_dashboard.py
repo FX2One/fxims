cb7992fb0c7ca0aed15ae30d389c73951d533e27
FX2One <dfoxik@gmail.com>
2022-08-10 15:33:32 +0200

    test default admin and dashboard after admin login to panel

--- fxecommerce/dashboard/tests/test_selenium_dashboard.py
+++ fxecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -1,6 +1,24 @@
 import pytest
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
 
 @pytest.mark.selenium
-def test_dashboard_admin_login(chrome_browser):
-    pass
+def test_new_admin_user(create_admin):
+    assert create_admin.username == "admin"
+
+@pytest.mark.selenium
+def test_dashboard_admin_login(live_server, chrome_browser, create_admin):
+    driver = chrome_browser
+    driver.get(f'{live_server.url}/admin/login/')
+
+    user_name = driver.find_element(By.XPATH, '//*[@id="id_username"]')
+    user_password = driver.find_element(By.XPATH, '//*[@id="id_password"]')
+    submit_button = driver.find_element(By.XPATH, '//input[@value="Log in"]')
+
+    user_name.send_keys(create_admin.username)
+    user_password.send_keys('password')
+    submit_button.send_keys(Keys.RETURN)
+
+    assert "Site administration" in driver.page_source
