f080dfa7ea275abc79ccea9d1109afb08f827d7b
FX2One <dfoxik@gmail.com>
2022-08-12 00:13:35 +0200

    new test for inventory category ,parametrized by CategoryFactory

---
+++ fxecommerce/inventory/tests/test_db_fixtures.py
@@ -0,0 +1,44 @@
+import pytest
+from fxecommerce.inventory import models
+
+
+"""regular parametrize test"""
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, category_name, description, image",
+    [
+        (1,'football','footbal','images/default.png'),
+        (10,'basketball','basketball','images/default.png'),
+        (101,'volleyball','volleyball','images/default.png')
+    ],
+)
+def test_inventory_category_dbfixture(
+  db, db_fixture_setup, id, category_name, description, image
+):
+    result = models.Category.objects.get(id=id)
+    assert result.category_name == category_name
+    assert result.description == description
+    assert result.image == image
+
+
+"""parametrize with FactoryBoy package"""
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "category_name, description, image",
+    [
+        ('football','footbal','images/default.png'),
+        ('basketball','basketball','images/default.png'),
+        ('volleyball','volleyball','images/default.png')
+    ],
+)
+def test_inventory_category_dbfixture_insert_fb(
+        db, category_factory, category_name, description, image
+):
+    result = category_factory.create(
+        category_name=category_name,
+        description=description,
+        image=image
+    )
+    assert result.category_name == category_name
+    assert result.description == description
+    assert result.image == image
