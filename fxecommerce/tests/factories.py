b0c7b8ae81a140efc549e58c8aa6a6dab76f8adf
FX2One <dfoxik@gmail.com>
2022-08-12 15:58:10 +0200

    replace lexify with factory.Sequence to autoincrement

--- fxecommerce/tests/factories.py
+++ fxecommerce/tests/factories.py
@@ -4,14 +4,15 @@ from faker import Faker
 from pytest_factoryboy import register
 from fxecommerce.inventory import models
 
+
 fake = Faker()
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Category
 
-    category_name = fake.lexify(text="category_name_?????")
-    description = fake.lexify(text="description_?????")
+    category_name = factory.Sequence(lambda x: f"category_name_{x}")
+    description = factory.Sequence(lambda y: f"description_{y}")
     image = "images/default.png"
 
 register(CategoryFactory)
