dbab8a6d11bc0fa2911e50a8348373af0fd55889
FX2One <dfoxik@gmail.com>
2022-08-16 15:37:05 +0200

    registering Product and Supplier models

--- fxecommerce/inventory/admin.py
+++ fxecommerce/inventory/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from fxecommerce.inventory.models import Category
+from fxecommerce.inventory.models import Category, Product, Supplier
 # Register your models here.
 
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Supplier)
