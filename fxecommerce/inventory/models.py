2761a09b69c7b211b50d51c7a353c4e5f67f53ac
FX2One <dfoxik@gmail.com>
2022-08-16 15:37:22 +0200

    added Product full model and empty Supplier model

--- fxecommerce/inventory/models.py
+++ fxecommerce/inventory/models.py
@@ -33,8 +33,98 @@ class Category(models.Model):
     )
 
     class Meta:
-        db_table = 'categories'
+        db_table = 'category'
         verbose_name_plural = _("Product categories")
 
     def __str__(self):
         return self.category_name
+
+
+class Product(models.Model):
+    product_id = models.AutoField(
+        verbose_name=_('Product ID'),
+        db_column='ProductID',
+        primary_key=True
+    )
+    product_name = models.CharField(
+        verbose_name=_('Product name'),
+        db_column='ProductName',
+        max_length=100,
+        null=False,
+        blank=False,
+        db_index=True,
+        help_text=_("format: required. max_length: 100")
+    )
+    description = models.TextField(
+        verbose_name=_('Description'),
+        db_column='Description',
+        null=False,
+        blank=False,
+        max_length=255,
+        help_text=_('format: required, max_length: 255')
+    )
+    supplier_id = models.ForeignKey(
+        Supplier,
+        db_column='SupplierID',
+        blank=True,
+        null=True,
+        db_index=True,
+        on_delete=models.CASCADE,
+    )
+    category_id = models.ForeignKey(
+        Category,
+        db_column='CategoryID',
+        blank=True,
+        null=True,
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+    quantity_per_unit = models.CharField(
+        verbose_name=_('Quantity per Unit'),
+        db_column='QuantityPerUnit',
+        max_length=20,
+        blank=True,
+        null=False
+    )
+    unit_price = models.DecimalField(
+        verbose_name=_('Unit price'),
+        db_column='UnitPrice',
+        blank=True,
+        null=True,
+        max_digits=19,
+        decimal_places=4
+    )
+    units_in_stock = models.SmallIntegerField(
+        verbose_name=_('Units in Stock'),
+        db_column='UnitsInStock',
+        blank=True,
+        null=True
+    )
+    units_on_order = models.SmallIntegerField(
+        verbose_name=_('Units on Order'),
+        db_column='UnitsOnOrder',
+        blank=True,
+        null=True
+    )
+    reorder_level = models.SmallIntegerField(
+        verbose_name=_('Reorder level'),
+        db_column='ReorderLevel',
+        blank=True,
+        null=True
+    )
+    discontinued = models.IntegerField(
+        _('Discontinued'),
+        db_column='Discontinued'
+    )
+
+    class Meta:
+        db_table = 'product'
+        verbose_name_plural = _("Products")
+
+
+class Supplier(models.Model):
+    pass
+
+    class Meta:
+        db_table = 'supplier'
+        verbose_name_plural = _("Suppliers")
