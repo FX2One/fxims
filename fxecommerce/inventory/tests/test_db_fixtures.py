e4989ab2b5d662293b1725518366109bf4492bba
FX2One <dfoxik@gmail.com>
2022-08-19 16:03:54 +0200

    applying utils methods into parametrize

--- fxecommerce/inventory/tests/test_db_fixtures.py
+++ fxecommerce/inventory/tests/test_db_fixtures.py
@@ -1,19 +1,23 @@
 import pytest
 from fxecommerce.inventory import models
-
+import json
+from utils import JsonLoadData, ConfigFixture
 
 """regular parametrize test"""
+"""asserts against db_category_fixture_id.json"""
 @pytest.mark.dbfixture
 @pytest.mark.parametrize(
     "category_id, category_name, description, image",
     [
-        (1,'football','All equipment related to Sports','default.png')
+        (1,'football','All equipment related to Sports','default.png'),
+        (2,'E-Games','Video games','default.png')
     ],
 )
 def test_inventory_category_dbfixture(
   db, django_database_fixture_setup, category_id, category_name, description, image
 ):
     result = models.Category.objects.get(category_id=category_id)
+    print(f'cat_id{result.category_id}=={result.category_name}')
     assert result.category_name == category_name
     assert result.description == description
     assert result.image == image
@@ -27,6 +31,7 @@ def test_inventory_category_dbfixture(
         ('football','All football equipment','default.png'),
         ('basketball','All basketball equipment','default.png'),
         ('volleyball','All volleyball equipment','default.png')
+
     ],
 )
 def test_inventory_category_dbfixture_insert_fb(
@@ -37,6 +42,7 @@ def test_inventory_category_dbfixture_insert_fb(
         description=description,
         image=image
     )
+    print(f'ticd: {result.category_name}')
     assert result.category_name == category_name
     assert result.description == description
     assert result.image == image
@@ -58,11 +64,115 @@ def test_inventory_category_dbfixture_insert_fb2(
         category_name=category_name,
         image=image
     )
-    print(result.category_name)
     assert result.category_name == category_name
     assert result.image == image
 
 
+def load_param_json(json_path):
+    with open(json_path) as f:
+        return json.load(f)
+
+abs_path = '../fixtures/db_supplier_fixture.json'
+
+
+def load_test_vals(abs_path,json_id):
+    load_test_case = load_param_json(abs_path)
+    return load_test_case[json_id]['fields'].values()
+
+jld = JsonLoadData()
+cf = ConfigFixture()
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    jld.load_keys(cf.SUPPLIER_FIXTURE),
+    [
+        ('Adeedas','Adee Das','Adeedas Company','Adee address','Munich','Bavaria','11-222','Germany','777-777-777','777-777-778','adeedas.com'),
+        ('Puma','Pumba ','Pumba Company','Pumba address','Berlin','Brandenburgia','21-122','Germany','888-777-777','888-777-778','pumba.com'),
+        load_test_vals(abs_path,0),
+        load_test_vals(abs_path,1),
+        jld.load_values(cf.SUPPLIER_FIXTURE,2)
+    ],
+)
+def test_inventory_supplier_dbfixture_insert(
+        db,supplier_factory,company_name,contact_name,contact_title,address,city,region,postal_code,country,phone,fax,homepage
+):
+    result = supplier_factory.create(
+        company_name=company_name,
+        contact_name=contact_name,
+        contact_title=contact_title,
+        address=address,
+        city=city,
+        region=region,
+        postal_code=postal_code,
+        country=country,
+        phone=phone,
+        fax=fax,
+        homepage=homepage
+    )
+    print(result.contact_title)
+    print(result.company_name)
+    print(result.city)
+    print(result.phone)
+    print(result.homepage)
+    assert result.company_name == company_name
+    assert result.contact_title == contact_title
+    assert result.city == city
+    assert result.region == region
+    assert result.postal_code == postal_code
+    assert result.country == country
+    assert result.phone == phone
+    assert result.fax == fax
+    assert result.homepage == homepage
+
+
+
+'''
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "company_name,contact_name,contact_title,address,city,region,postal_code,country,phone,fax,homepage",
+    [
+        ('Adidas','Adi Das','Adidas Company','Adi address','Munich','Bawaria','11-222','Germany','777-777-777','777-777-778','adidas.com'),
+        ('Nike','Mark Nikovic'),
+        ('Puma','Pum Anters'),
+        ('Reebok','Reebeca Oklah')
+    ],
+)
+'''
+'''@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "db,supplier_factory,company_name,contact_name,contact_title,address,city,region,postal_code,country,phone,fax,homepage",
+    [
+        ()
+    ]
+):
+def test_inventory_supplier_dbfixture_insert(
+    db,
+    supplier_factory,
+    company_name,
+    contact_name,
+    contact_title,
+    address,
+    city,
+    region,
+    postal_code,
+    country,
+    phone,
+    fax,
+    homepage
+):
+    result = supplier_factory.create(
+        company_name=company_name,
+        contact_name=contact_name,
+        contact_title=contact_title,
+        address=address,
+        city=city,
+        region=region,
+        postal_code=postal_code,
+        country=country,
+        phone=phone,
+        fax=fax,
+        homepage=homepage
+    )'''
 
 
 
