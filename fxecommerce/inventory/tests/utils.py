312b109d803b74fb1753d22a3acdfe6edbdb4bda
FX2One <dfoxik@gmail.com>
2022-08-22 15:51:04 +0200

    typ hints added to functions

--- fxecommerce/inventory/tests/utils.py
+++ fxecommerce/inventory/tests/utils.py
@@ -1,29 +1,25 @@
 import json
+from typing import Tuple, List
 
 
 class JsonLoadData:
 
-    def load_param_json(self,json_path):
+    def load_param_json(self, json_path: str) -> List:
         with open(json_path) as f:
             return json.load(f)
 
-
-    def load_values(self,json_path,json_index):
+    def load_values(self, json_path: str, json_index: int) -> Tuple:
         load_test_case = self.load_param_json(json_path)
         return tuple(list(load_test_case[json_index]['fields'].values()))
 
-
-    def load_keys(self,json_path):
+    def load_keys(self, json_path: str) -> str:
         load_test_case = self.load_param_json(json_path)
         load_list = list(load_test_case[0]['fields'].keys())
-        list_to_str = ','.join(map(str,load_list))
+        list_to_str = ','.join(map(str, load_list))
         return list_to_str
 
 
 class ConfigFixture:
-
     SUPPLIER_FIXTURE = '../fixtures/db_supplier_fixture.json'
-
-
-
+    PRODUCT_FIXTURE = '../fixtures/db_product_fixture.json'
 
