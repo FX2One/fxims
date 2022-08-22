4fef7a831c6db8f6a71c919cdc7f68eb23fd6d7e
FX2One <dfoxik@gmail.com>
2022-08-22 23:20:39 +0200

    changes to factories

--- fxecommerce/tests/factories.py
+++ fxecommerce/tests/factories.py
@@ -4,7 +4,6 @@ import string
 from faker import Faker
 from pytest_factoryboy import register
 from fxecommerce.inventory import models
-import factory.fuzzy
 
 fake = Faker()
 
@@ -32,26 +31,27 @@ class SupplierFactory(factory.django.DjangoModelFactory):
     country = factory.Faker('country')
     phone = factory.Faker('phone_number')
     fax = factory.Faker('fax')
-    homepage = fake.lexify(text="homepage_??????")
+    homepage = fake.lexify(text="www.???????.com")
 
 
 
 
-'''class ProductFactory(factory.django.DjangoModelFactory):
+class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Product
 
-    product_name = factory.Sequence(lambda x: f"product_name_{x}")
-    description = factory.Sequence(lambda x: f"description_{x} with some long text")
-    supplier_id = factory.Sequence(lambda x: x)
-    category_id = factory.Sequence(lambda x: x)
-    quantity_per_unit = factory.Sequence(lambda x, y: f"amount of {x} in {y} boxes")
-    unit_price = factory.Sequence(lambda x: x)
-    units_in_stock = factory.Sequence(lambda x: x)
-    units_on_order = factory.Sequence(lambda x: x)
-    reorder_level = factory.Sequence(lambda x: x)
-    discontinued = factory.Sequence(lambda x: x)'''
+    product_name = fake.lexify(text='product_??????????')
+    description = fake.lexify(text='description_??????????')
+    supplier_id = fake.random_digit_not_null()
+    category_id = fake.random_digit_not_null()
+    quantity_per_unit = fake.lexify(text='amount ?? in 1 box which contains ?? on pallete')
+    unit_price = fake.random_number(digits=5, fix_len=False)
+    units_in_stock = fake.random_number(digits=5, fix_len=False)
+    units_on_order = fake.random_number(digits=5, fix_len=False)
+    reorder_level = fake.random_number(digits=5, fix_len=False)
+    discontinued = fake.random_int(min=0, max=1, step=1)
 
 
 register(CategoryFactory)
 register(SupplierFactory)
+register(ProductFactory)
