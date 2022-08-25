ea5c0d8e246e9bf4480c82e0d7a5323d1f9541a2
FX2One <dfoxik@gmail.com>
2022-08-26 01:00:05 +0200

    model reorder structure

--- fxecommerce/inventory/models.py
+++ fxecommerce/inventory/models.py
@@ -4,6 +4,312 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
+class Region(models.Model):
+    region_id = models.PositiveSmallIntegerField(
+        verbose_name=_('Region id'),
+        db_column='RegionID',
+        primary_key=True
+    )
+    region_description = models.CharField(
+        verbose_name=_('Region description'),
+        db_column='RegionDescription',
+        max_length=50
+    )
+
+    class Meta:
+        db_table = 'region'
+        verbose_name_plural = _('Regions')
+
+
+class Territory(models.Model):
+    territory_id = models.CharField(
+        verbose_name=_('Territory id'),
+        db_column='TerritoryID',
+        primary_key=True,
+        max_length=20
+    )
+    territory_description = models.CharField(
+        verbose_name=_('Territory description'),
+        db_column='TerritoryDescription',
+        max_length=50
+    )
+    region_id = models.ForeignKey(
+        Region,
+        db_column='RegionID',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        db_table = 'territory'
+        verbose_name_plural=_('Territories')
+
+
+class Employee(models.Model):
+    employee_id = models.AutoField(
+        verbose_name=_('Employee id'),
+        db_column='EmployeeID',
+        primary_key=True,
+    )
+    last_name = models.CharField(
+        verbose_name=_('Last name'),
+        db_column='LastName',
+        max_length=20,
+        blank=False,
+        null=False,
+        db_index=True
+    )
+    first_name = models.CharField(
+        verbose_name=_('First name'),
+        db_column='FirstName',
+        max_length=10,
+        blank=False,
+        null=False
+    )
+    title = models.CharField(
+        verbose_name=_('title'),
+        db_column='Title',
+        max_length=30,
+        blank=False,
+        null=False
+    )
+    title_of_courtesy = models.CharField(
+        verbose_name=_('Title of Courtesy'),
+        db_column='TitleOfCourtesy',
+        max_length=25,
+        blank=True,
+        null=False
+    )
+    birth_date = models.DateTimeField(
+        verbose_name=_('Birth date'),
+        db_column='BirthDate',
+        blank=True,
+        null=True
+    )
+    hire_date = models.DateTimeField(
+        verbose_name=_('Hire date'),
+        db_column='HireDate',
+        blank=True,
+        null=True
+    )
+    address = models.CharField(
+        verbose_name=_('address'),
+        db_column='Address',
+        max_length=60,
+        blank=False,
+        null=False
+    )
+    city = models.CharField(
+        verbose_name=_('City'),
+        db_column='City',
+        max_length=15,
+        blank=False,
+        null=False
+    )
+    region = models.CharField(
+        verbose_name=_('Region'),
+        db_column='Region',
+        max_length=15,
+        blank=True,
+        null=False
+    )
+    postal_code = models.CharField(
+        verbose_name=_('Postal code'),
+        db_column='PostalCode',
+        max_length=10,
+        blank=False,
+        null=False,
+        db_index=True
+    )
+    country = models.CharField(
+        verbose_name=_('Country'),
+        db_column='Country',
+        max_length=15,
+        blank=False,
+        null=False
+    )
+    home_phone = models.CharField(
+        verbose_name=_('Home phone'),
+        db_column='HomePhone',
+        max_length=24,
+        blank=False,
+        null=False
+    )
+    extension = models.CharField(
+        verbose_name=_('Extension'),
+        db_column='Extension',
+        max_length=4,
+        blank=False,
+        null=False
+    )
+    photo = models.BinaryField(
+        verbose_name=_('Photo'),
+        db_column='Photo',
+        blank=True,
+        null=True
+    )
+    notes = models.TextField(
+        verbose_name=_('Notes'),
+        db_column='Notes',
+        blank=True,
+        null=False
+    )
+    reports_to = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        db_column='ReportsTo',
+        on_delete=models.PROTECT,
+    )
+    photo_path = models.CharField(
+        verbose_name=_('Photo path'),
+        db_column='PhotoPath',
+        max_length=255,
+        blank=True,
+        null=False
+    )
+    territories = models.ManyToManyField(
+        Territory,
+        verbose_name=_('Territories'),
+        db_table='EmployeeTerritories',
+        blank=True,
+    )
+
+    class Meta:
+        db_table = 'employee'
+        verbose_name_plural = _('Employees')
+
+
+class Shipper(models.Model):
+    shipper_id = models.AutoField(
+        verbose_name=_('Shipper ID'),
+        db_column='ShipperID',
+        primary_key=True
+    )
+    company_name = models.CharField(
+        _('Company name'),
+        db_column='CompanyName',
+        max_length=40
+    )
+    phone = models.CharField(
+        _('Phone'),
+        db_column='Phone',
+        max_length=24,
+        blank=False,
+        null=False
+    )
+
+    class Meta:
+        db_table = 'shipper'
+        verbose_name_plural = _('Shippers')
+
+
+class CustomerDemographics(models.Model):
+    customer_type_id = models.CharField(
+        varbose_name=_('Customer typeID'),
+        db_column='CustomerTypeID',
+        primary_key=True,
+        max_length=5
+    )
+    customer_desc = models.TextField(
+        verbose_name=_('Customer description'),
+        db_column='CustomerDesc',
+        blank=False,
+        null=True
+    )
+    class Meta:
+        db_table = 'customerdemographics'
+
+
+class Customer(models.Model):
+    customer_id = models.CharField(
+        verbose_name=_('Customer ID'),
+        db_column='CustomerID',
+        primary_key=True,
+        max_length=5
+    )
+    company_name = models.CharField(
+        verbose_name=_('Company name'),
+        db_column='CompanyName',
+        max_length=40
+    )
+    contact_name = models.CharField(
+        verbose_name=_('Contact name'),
+        db_column='ContactName',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    contact_title = models.CharField(
+        verbose_name=_('Contact title'),
+        db_column='ContactTitle',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    address = models.CharField(
+        verbose_name=_('Address'),
+        db_column='Address',
+        max_length=60,
+        blank=True,
+        null=True
+    )
+    city = models.CharField(
+        verbose_name=_('City'),
+        db_column='City',
+        max_length=15,
+        blank=True,
+        null=True
+    )
+    region = models.CharField(
+        verbose_name=_('Region'),
+        db_column='Region',
+        max_length=15,
+        blank=True,
+        null=True
+    )
+    postal_code = models.CharField(
+        verbose_name=_('Postal code'),
+        db_column='PostalCode',
+        max_length=10,
+        blank=True,
+        null=True
+    )
+    country = models.CharField(
+        verbose_name=_('Country'),
+        db_column='Country',
+        max_length=15,
+        blank=True,
+        null=True
+    )
+    phone = models.CharField(
+        verbose_name=_('Phone'),
+        db_column='Phone',
+        max_length=24,
+        blank=True,
+        null=True
+    )
+    fax = models.CharField(
+        verbose_name=_('Fax'),
+        db_column='Fax',
+        max_length=24,
+        blank=True,
+        null=True
+    )
+    customer_customer_demo = models.ManyToManyField(
+        CustomerDemographics,
+        verbose_name=_('Customer customer demo'),
+        db_table='CustomerCustomerDemo',
+        blank=True,
+    )
+
+    class Meta:
+        db_table = 'customer'
+        verbose_name_plural = _("Customers")
+
+    def __str__(self):
+        return self.company_name
+
+
 class Category(models.Model):
     category_id = models.AutoField(
         verbose_name=_('Category ID'),
@@ -358,305 +664,16 @@ class OrderDetails(models.Model):
         db_table = 'order_details'
 
 
-class Customer(models.Model):
-    customer_id = models.CharField(
-        verbose_name=_('Customer ID'),
-        db_column='CustomerID',
-        primary_key=True,
-        max_length=5
-    )
-    company_name = models.CharField(
-        verbose_name=_('Company name'),
-        db_column='CompanyName',
-        max_length=40
-    )
-    contact_name = models.CharField(
-        verbose_name=_('Contact name'),
-        db_column='ContactName',
-        max_length=30,
-        blank=True,
-        null=True
-    )
-    contact_title = models.CharField(
-        verbose_name=_('Contact title'),
-        db_column='ContactTitle',
-        max_length=30,
-        blank=True,
-        null=True
-    )
-    address = models.CharField(
-        verbose_name=_('Address'),
-        db_column='Address',
-        max_length=60,
-        blank=True,
-        null=True
-    )
-    city = models.CharField(
-        verbose_name=_('City'),
-        db_column='City',
-        max_length=15,
-        blank=True,
-        null=True
-    )
-    region = models.CharField(
-        verbose_name=_('Region'),
-        db_column='Region',
-        max_length=15,
-        blank=True,
-        null=True
-    )
-    postal_code = models.CharField(
-        verbose_name=_('Postal code'),
-        db_column='PostalCode',
-        max_length=10,
-        blank=True,
-        null=True
-    )
-    country = models.CharField(
-        verbose_name=_('Country'),
-        db_column='Country',
-        max_length=15,
-        blank=True,
-        null=True
-    )
-    phone = models.CharField(
-        verbose_name=_('Phone'),
-        db_column='Phone',
-        max_length=24,
-        blank=True,
-        null=True
-    )
-    fax = models.CharField(
-        verbose_name=_('Fax'),
-        db_column='Fax',
-        max_length=24,
-        blank=True,
-        null=True
-    )
-    customer_customer_demo = models.ManyToManyField(
-        CustomerDemographics,
-        verbose_name=_('Customer customer demo'),
-        db_table='CustomerCustomerDemo',
-        blank=True,
-    )
-
-    class Meta:
-        db_table = 'customer'
-        verbose_name_plural = _("Customers")
-
-    def __str__(self):
-        return self.company_name
-
-class CustomerDemographics(models.Model):
-    customer_type_id = models.CharField(
-        varbose_name=_('Customer typeID'),
-        db_column='CustomerTypeID',
-        primary_key=True,
-        max_length=5
-    )
-    customer_desc = models.TextField(
-        verbose_name=_('Customer description'),
-        db_column='CustomerDesc',
-        blank=True,
-        null=True
-    )
-    class Meta:
-        db_table = 'customerdemographics'
 
 
-class Shipper(models.Model):
-    shipper_id = models.AutoField(
-        verbose_name=_('Shipper ID'),
-        db_column='ShipperID',
-        primary_key=True
-    )
-    company_name = models.CharField(
-        _('Company name'),
-        db_column='CompanyName',
-        max_length=40
-    )
-    phone = models.CharField(
-        _('Phone'),
-        db_column='Phone',
-        max_length=24,
-        blank=False,
-        null=False
-    )
-
-    class Meta:
-        db_table = 'shipper'
-        verbose_name_plural = _('Shippers')
-
-class Employee(models.Model):
-    employee_id = models.AutoField(
-        verbose_name=_('Employee id'),
-        db_column='EmployeeID',
-        primary_key=True,
-    )
-    last_name = models.CharField(
-        verbose_name=_('Last name'),
-        db_column='LastName',
-        max_length=20,
-        blank=False,
-        null=False,
-        db_index=True
-    )
-    first_name = models.CharField(
-        verbose_name=_('First name'),
-        db_column='FirstName',
-        max_length=10,
-        blank=False,
-        null=False
-    )
-    title = models.CharField(
-        verbose_name=_('title'),
-        db_column='Title',
-        max_length=30,
-        blank=False,
-        null=False
-    )
-    title_of_courtesy = models.CharField(
-        verbose_name=_('Title of Courtesy'),
-        db_column='TitleOfCourtesy',
-        max_length=25,
-        blank=True,
-        null=True
-    )
-    birth_date = models.DateTimeField(
-        verbose_name=_('Birth date'),
-        db_column='BirthDate',
-        blank=True,
-        null=True
-    )
-    hire_date = models.DateTimeField(
-        verbose_name=_('Hire date'),
-        db_column='HireDate',
-        blank=True,
-        null=True
-    )
-    address = models.CharField(
-        verbose_name=_('address'),
-        db_column='Address',
-        max_length=60,
-        blank=False,
-        null=False
-    )
-    city = models.CharField(
-        verbose_name=_('City'),
-        db_column='City',
-        max_length=15,
-        blank=False,
-        null=False
-    )
-    region = models.CharField(
-        verbose_name=_('Region'),
-        db_column='Region',
-        max_length=15,
-        blank=True,
-        null=True
-    )
-    postal_code = models.CharField(
-        verbose_name=_('Postal code'),
-        db_column='PostalCode',
-        max_length=10,
-        blank=False,
-        null=False,
-        db_index=True
-    )
-    country = models.CharField(
-        verbose_name=_('Country'),
-        db_column='Country',
-        max_length=15,
-        blank=False,
-        null=False
-    )
-    home_phone = models.CharField(
-        verbose_name=_('Home phone'),
-        db_column='HomePhone',
-        max_length=24,
-        blank=False,
-        null=False
-    )
-    extension = models.CharField(
-        verbose_name=_('Extension'),
-        db_column='Extension',
-        max_length=4,
-        blank=False,
-        null=False
-    )
-    photo = models.BinaryField(
-        verbose_name=_('Photo'),
-        db_column='Photo',
-        blank=True,
-        null=True
-    )
-    notes = models.TextField(
-        verbose_name=_('Notes'),
-        db_column='Notes',
-        blank=True,
-        null=True
-    )
-    reports_to = models.ForeignKey(
-        'self',
-        null=True,
-        blank=True,
-        db_column='ReportsTo',
-        on_delete=models.PROTECT,
-    )
-    photo_path = models.CharField(
-        verbose_name=_('Photo path'),
-        db_column='PhotoPath',
-        max_length=255,
-        blank=True,
-        null=True
-    )
-    territories = models.ManyToManyField(
-        Territory,
-        verbose_name=_('Territories'),
-        db_table='EmployeeTerritories',
-        blank=True,
-    )
-
-    class Meta:
-        db_table = 'employee'
-        verbose_name_plural = _('Employees')
 
 
-class Territory(models.Model):
-    territory_id = models.CharField(
-        verbose_name=_('Territory id'),
-        db_column='TerritoryID',
-        primary_key=True,
-        max_length=20
-    )
-    territory_description = models.CharField(
-        verbose_name=_('Territory description'),
-        db_column='TerritoryDescription',
-        max_length=50
-    )
-    region_id = models.ForeignKey(
-        Region,
-        db_column='RegionID',
-        on_delete=models.CASCADE
-    )
-
-    class Meta:
-        db_table = 'territory'
-        verbose_name_plural=_('Territories')
 
 
-class Region(models.Model):
-    region_id = models.PositiveSmallIntegerField(
-        verbose_name=_('Region id'),
-        db_column='RegionID',
-        primary_key=True
-    )
-    region_description = models.CharField(
-        verbose_name=_('Region description'),
-        db_column='RegionDescription',
-        max_length=50
-    )
 
-    class Meta:
-        db_table = 'region'
-        verbose_name_plural = _('Regions')
+
+
+
+
+
+
