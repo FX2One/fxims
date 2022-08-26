fcd730390a5c1920c08054e9eedee4a1a09fd9b7
FX2One <dfoxik@gmail.com>
2022-08-26 16:02:41 +0200

    registered newly added models

--- fxecommerce/inventory/admin.py
+++ fxecommerce/inventory/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from fxecommerce.inventory.models import Category, Product, Supplier
+from fxecommerce.inventory.models import Category, Product, Supplier,Order,CustomerDemographics, Customer, Territory, Region, OrderDetails, Shipper, Employee
 
 # Register your models here.
-
+admin.site.register(Employee)
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Supplier)
+admin.site.register(Customer)
+admin.site.register(Shipper)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
+admin.site.register(CustomerDemographics)
+admin.site.register(Territory)
+admin.site.register(Region)
+
