cccfba46beaff63d56e8857aaf22c40109ad038c
FX2One <dfoxik@gmail.com>
2022-08-31 08:52:31 +0200

    config fixtures added

--- fxecommerce/inventory/tests/utils.py
+++ fxecommerce/inventory/tests/utils.py
@@ -25,4 +25,11 @@ class ConfigFixture:
     SUPPLIER_FIXTURE = f'{path}db_supplier_fixture.json'
     PRODUCT_FIXTURE = f'{path}db_product_fixture.json'
     ORDER_FIXTURE = f'{path}db_order_fixture.json'
+    TERRITORY_FIXTURE = f'{path}db_territory_fixture.json'
+    EMPLOYEE_FIXTURE = f'{path}db_employee_fixture.json'
+    SHIPPER_FIXTURE = f'{path}db_shipper_fixture.json'
+    CUSTOMER_FIXTURE = f'{path}db_customer_fixture.json'
+    ORDER_DETAILS_FIXTURE = f'{path}db_order_details_fixture.json'
+    REGION_FIXTURE = f'{path}db_region_fixture.json'
+
 
