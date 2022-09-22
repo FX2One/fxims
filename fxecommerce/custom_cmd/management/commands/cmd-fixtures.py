c9b93a1a9554cdfebbf037718a537115f1146aef
FX2One <dfoxik@gmail.com>
2022-09-22 14:07:41 +0200

    call_command handler updated with remaining fixtures

--- fxecommerce/custom_cmd/management/commands/cmd-fixtures.py
+++ fxecommerce/custom_cmd/management/commands/cmd-fixtures.py
@@ -1,12 +1,20 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         call_command("makemigrations")
         call_command("migrate")
-        call_command("loaddata","db_admin_fixture.json")
-        call_command("loaddata","db_category_fixture_id.json")
-        call_command("loaddata","db_supplier_fixture.json")
-        call_command("loaddata","db_product_fixture.json")
-        call_command("loaddata","db_order_fixture.json")
+        call_command('loaddata', 'db_admin_fixture.json')
+        call_command('loaddata', 'db_region_fixture.json')
+        call_command('loaddata', 'db_territory_fixture.json')
+        call_command('loaddata', 'db_employee_fixture.json')
+        call_command('loaddata', 'db_shipper_fixture.json')
+        call_command('loaddata', 'db_customer_fixture.json')
+        call_command('loaddata', 'db_customer_demo_fixture.json')
+        call_command('loaddata', 'db_category_fixture.json')
+        call_command('loaddata', 'db_supplier_fixture.json')
+        call_command('loaddata', 'db_product_fixture.json')
+        call_command('loaddata', 'db_order_fixture.json')
+        call_command('loaddata', 'db_order_details_fixture.json')
