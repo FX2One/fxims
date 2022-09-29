04f6caed118e39525fbd263511940e0ec12d6eef
FX2One <dfoxik@gmail.com>
2022-09-29 14:39:00 +0200

    change base.html as base to extend other template ,change base in home view to index.html

--- fxecommerce/inventory/urls.py
+++ fxecommerce/inventory/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name="inventory"
+app_name = "inventory"
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('categories/', views.categories),
+    path('products/', views.products)
 ]
--- fxecommerce/inventory/views.py
+++ fxecommerce/inventory/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Category, Product
+
 
 def home(request):
-    return render(request, "base.html")
+    return render(request, "index.html")
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, "categories.html", {"categories": categories})
 
 
