e5064bf05eab75971102442024683685ba449b45
FX2One <dfoxik@gmail.com>
2022-10-04 12:19:40 +0200

    function based views commented out in favour of class based views

--- fxecommerce/inventory/views.py
+++ fxecommerce/inventory/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Category, Product
 
 
 def home(request):
     return render(request, "index.html")
 
-def products(request):
+
+'''def products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
 
+
 def categories(request):
     categories = Category.objects.all()
-    return render(request, "categories.html", {"categories": categories})
+    return render(request, "categories.html", {"categories": categories})'''
 
 
+class ProductListView(ListView):
+    model = Product
+    template_name = "inventory/product_list.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "inventory/product_detail.html"
