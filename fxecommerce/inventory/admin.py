65e4e8d4caeff69bd17c67c390bd0df024cc0e3d
FX2One <dfoxik@gmail.com>
2022-10-04 12:17:16 +0200

    register ProductAdmin

--- fxecommerce/inventory/admin.py
+++ fxecommerce/inventory/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from fxecommerce.inventory.models import Category, Product, Supplier,Order,CustomerDemographics, Customer, Territory, Region, OrderDetails, Shipper, Employee
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("product_name","product_id")
+    prepopulated_fields = {"slug": ("product_name",)}
+
 # Register your models here.
 admin.site.register(Employee)
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Supplier)
 admin.site.register(Customer)
 admin.site.register(Shipper)
@@ -13,4 +18,7 @@ admin.site.register(OrderDetails)
 admin.site.register(CustomerDemographics)
 admin.site.register(Territory)
 admin.site.register(Region)
+
+
+
 
