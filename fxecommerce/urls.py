7031f6b6454dca74dd59bc24c0839f261c8d01fd
FX2One <dfoxik@gmail.com>
2022-10-04 12:20:43 +0200

    change from /inv to /inventory in url dispatcher

--- fxecommerce/urls.py
+++ fxecommerce/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inv/', include("fxecommerce.inventory.urls", namespace="inventory")),
-]
+    path("inventory/", include("fxecommerce.inventory.urls", namespace="inventory")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
