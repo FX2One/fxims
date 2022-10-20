4cfe4a8d7f74fd665ed57a3eba357f0d7c827ab0
FX2One <dfoxik@gmail.com>
2022-10-21 01:32:54 +0200

    serve static and media to settings + DEBUG guard to serve statics in urls

--- fxecommerce/config/urls.py
+++ fxecommerce/config/urls.py
@@ -8,5 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("inventory.urls", namespace="inventory")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
