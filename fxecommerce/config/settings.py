20d757ce5c65c30b112c72def9c62b8dc40b60a1
FX2One <dfoxik@gmail.com>
2022-10-28 15:36:33 +0200

    remove redirect url for logout to make it have its own redirect page

--- fxecommerce/config/settings.py
+++ fxecommerce/config/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = 'inventory:home'
-LOGOUT_REDIRECT_URL = 'inventory:home'
+# LOGOUT_REDIRECT_URL = 'inventory:home'
 
 # Application definition
 
