425f562adb916292bff199df971e7da59fc88f27
FX2One <dfoxik@gmail.com>
2022-11-30 09:18:04 +0100

    add: 3 random users to be added

--- fxecommerce/apps/custom_cmd/management/commands/cmd-fixtures.py
+++ fxecommerce/apps/custom_cmd/management/commands/cmd-fixtures.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
         call_command("makemigrations")
         call_command("migrate")
         call_command('loaddata', 'db_admin_fixture.json') #basic admin creation admin@admin.com ,admin1
+        call_command('loaddata', 'db_user_fixture.json') #basic admin creation admin@admin.com ,admin1
         call_command('loaddata', 'db_region_fixture.json')
         call_command('loaddata', 'db_territory_fixture.json')
         call_command('loaddata', 'db_employee_fixture.json')
