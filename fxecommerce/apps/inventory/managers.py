60aa5ca2ad8dbf5e7da6c2b1ee16cdf103e3ed8c
FX2One <dfoxik@gmail.com>
2022-12-08 22:35:01 +0100

    add: pagination by value/items display per page
    add: skeleton for managers for product/employee/order and category models
    
    remove: customer model
    
    remove: customer from register

---
+++ fxecommerce/apps/inventory/managers.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class EmployeeManager(models.Manager):
+    pass
+
+
+class ProductManager(models.Manager):
+    pass
+
+
+class CategoryManager(models.Manager):
+    pass
+
+
+class OrderManager(models.Manager):
+    pass
+
+
+
+
