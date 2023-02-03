d4cec1534f812d5175160c3883f601999362aa93
FX2One <dfoxik@gmail.com>
2023-02-03 11:38:19 +0100

    ADD: new url for category create view

--- fxecommerce/apps/inventory/urls.py
+++ fxecommerce/apps/inventory/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import (ProductListView, ProductDetailView,
                     ProductCreateView, ProductUpdateView, ProductDeleteView,
                     EmployeeListView, EmployeeDetailView, OrderDetailsListView,
-                    OrderSpecificationView, CategoryListView)
+                    OrderSpecificationView, CategoryListView, CategoryCreateView)
 
 app_name = 'inventory'
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path("employee/", EmployeeListView.as_view(), name="employee"),
     path("employee/<slug:slug>", EmployeeDetailView.as_view(),name="employee_detail"),
     path("category/", CategoryListView.as_view(), name="category"),
+    path("category/create", CategoryCreateView.as_view(), name="category_new"),
     path("order/", OrderDetailsListView.as_view(), name="order"),
     path("order/<int:pk>", OrderSpecificationView.as_view(), name="order_detail"),
     path("products/", ProductListView.as_view(), name="product_list"),
