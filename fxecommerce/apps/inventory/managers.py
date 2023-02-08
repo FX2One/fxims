c5980d7162ba909b715391de781c4463995220cc
FX2One <dfoxik@gmail.com>
2023-02-08 14:36:17 +0100

    REMOVE: Employee QuerySet and Manager moved to users

--- fxecommerce/apps/inventory/managers.py
+++ fxecommerce/apps/inventory/managers.py
@@ -2,26 +2,6 @@ from django.db import models
 from django.db.models import Q
 
 
-class EmployeeQuerySet(models.QuerySet):
-    def search(self, search_query):
-        if search_query:
-            return self.filter(
-                Q(first_name__icontains=search_query) |
-                Q(last_name__icontains=search_query)
-            )
-        return self
-
-class EmployeeManager(models.Manager):
-    def get_queryset(self):
-        return EmployeeQuerySet(self.model, using=self._db)
-
-    def search(self, search_query):
-        return self.get_queryset().search(search_query)
-
-    def get_paginate_by(self, queryset):
-        return self.request.GET.get("paginate_by", self.paginate_by)
-
-
 class ProductQuerySet(models.QuerySet):
     def search(self, search_query):
         if search_query:
