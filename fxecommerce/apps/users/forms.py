805f0a4933346fb1d45720a36800da24dc03afc8
FX2One <dfoxik@gmail.com>
2023-02-08 14:29:17 +0100

    UPDATE: change Models from UserProfile to Customer

--- fxecommerce/apps/users/forms.py
+++ fxecommerce/apps/users/forms.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import User, UserProfile
+from .models import User, Customer
 from django import forms
 
 '''subclass UserCreationForm to match new CustomUser'''
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('email',)
+        fields = ('email','user_type')
 
 
 '''subclass UserCreationForm to match new CustomUser'''
@@ -22,14 +22,9 @@ class CustomUserUpdateForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'email']
 
 
-'''class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['image']'''
-
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
-        model = UserProfile
+        model = Customer
         fields = [
             'image',
             'company_name',
