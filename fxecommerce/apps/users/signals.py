0877df7835b3d5179c91123aff3c6cb311a44a82
FX2One <dfoxik@gmail.com>
2023-02-08 14:26:14 +0100

    UPDATE: create object and add group based on user_type

--- fxecommerce/apps/users/signals.py
+++ fxecommerce/apps/users/signals.py
@@ -1,14 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, UserProfile
+from .models import User, Customer, Employee
 
 from django.contrib.auth.models import Group
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+    if created and instance.user_type == 1:
+        Employee.objects.create(user=instance)
+    elif created and instance.user_type == 4:
+        Customer.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
@@ -16,7 +18,11 @@ def save_user_profile(sender, instance, **kwargs):
     if instance.user_type == 4:
         group = Group.objects.get(name='Customer')
         instance.groups.add(group)
-    instance.profile.save()
+        instance.customer.save()
+    else:
+        group = Group.objects.get(name='Employee')
+        instance.groups.add(group)
+        instance.employee.save()
 
 
 
