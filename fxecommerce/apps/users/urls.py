f4ddb41a7a4287ca44c5f77ca7d55013504ca056
FX2One <dfoxik@gmail.com>
2023-03-07 22:38:23 +0100

    UPDATE: add urls for customer list and detail

--- fxecommerce/apps/users/urls.py
+++ fxecommerce/apps/users/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('profile/update/employee', views.employee_update, name='employee_update'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path("employee/", EmployeeListView.as_view(), name="employee"),
-    path("employee/<slug:slug>", EmployeeDetailView.as_view(),name="employee_detail"),
-    path("customer", CustomerListView.as_view(), name="customer"),
-    path("customer/<slug:slug>", CustomerDetailView.as_view(), name="customer_detail"),
+    path('employee/', EmployeeListView.as_view(), name='employee'),
+    path('employee/<slug:slug>', EmployeeDetailView.as_view(),name='employee_detail'),
+    path('customer', CustomerListView.as_view(), name='customer'),
+    path('customer/<slug:slug>', CustomerDetailView.as_view(), name='customer_detail'),
 ]
