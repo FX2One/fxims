8acdf985b43b63e05c3fe8ee42c0b0252dffb70f
FX2One <dfoxik@gmail.com>
2023-04-03 12:03:32 +0200

    ADD: permission to remove 'add' option for Order which is autocreated by another object, UPDATE: read-only fields that cannot be changed for already existing fields in object
    DELETE: fields for order_id whereas Order is created through OrderDetails, product_id to prevent change in admin panel when label is set
    
    UPDATE: OrderDetails FK to Order to PROTECT from CASCADE,
    UPDATE: OrderDetails clean method to check against existing quantity versus change while obj is being manipulated,
    DELETE: unecessary faulty code,
    ADD: two new signals for post_delete and post_save action while manipulating the instance.
    
    ADD: form validation for for units_in_stock and units_on_order to have the same functionality in both admin and the view
    
    UPDATE: change visibility of Order ID while creating ,which disables chosing it according to autocreate option
    
    UPDATE: orders_edit for both employee and customer user type ,hide unecessary fields and display only fields values to show user what that is

--- fxecommerce/apps/inventory/views.py
+++ fxecommerce/apps/inventory/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .mixins import GroupRequiredMixin
 from .forms import OrderDetailsForm, ProductForm, CategoryForm
 from django import forms
-from django.contrib import messages
 from users.models import User
 
 class HomeView(TemplateView):
@@ -88,6 +87,29 @@ class OrderDetailsUpdateView(GroupRequiredMixin, LoginRequiredMixin, UpdateView)
         context = super().get_context_data(**kwargs)
         context['form'] = self.get_form()
         return context
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+
+        # change = False (object being created)
+        # change = True (object exists and is being updated)
+        change = False if obj.pk is None else True
+
+        if change:
+            original_obj = OrderDetails.objects.get(pk=obj.pk)
+            quantity_diff = original_obj.quantity - obj.quantity
+
+            if quantity_diff > 0:
+                obj.product_id.units_in_stock += abs(quantity_diff)
+                obj.product_id.units_on_order -= abs(quantity_diff)
+            elif quantity_diff < 0:
+                obj.product_id.units_in_stock -= abs(quantity_diff)
+                obj.product_id.units_on_order += abs(quantity_diff)
+
+            obj.product_id.save(update_fields=['units_in_stock', 'units_on_order'])
+
+        obj.save()
+        return super().form_valid(form)
 
 
 class OrderDetailsDeleteView(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
